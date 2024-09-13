abca21b2866b148722e149de46d122409d69f3ac
PericlesDarth <aanonimov210@gmail.com>
2024-09-13 23:47:32 +0500

    Added Task6

---
+++ Tyuiu.KozachokNA.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿namespace Tyuiu.KozachokNA.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static int AdditionArray(int[] numbers)
+        {
+            int res = 0;
+            foreach (int number in numbers)
+            {
+                res += number;
+            }
+            return res;
+        }
+        public static int SubstractionArray(int[] numbers)
+        {
+            int res = 0;
+            int index = 0;
+
+            while (index < numbers.Length)
+            {
+                res -= numbers[index];
+                index++;
+            }
+            return res;
+        }
+        public static int MultiplicationArray(int[] numbers)
+        {
+            int res = 1;
+            int index = 0;
+            do
+            {
+                res *= numbers[index];
+                index++;
+            } while (index < numbers.Length);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.KozachokNA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KozachokNA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KozachokNA.Sprint0.Task6.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numbers));
+            Console.WriteLine("Разность элементов массива = " + DataService.SubstractionArray(numbers));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numbers));
+
+            Console.ReadKey();
+        }
+    }
+}
